b372ed9f8b45dbac4ef6e92ae6c55d1b9f0d689c
successiveprince <prince.kumar1@successive.tech>
2023-10-26 10:05:09 +0530

    Visual Studio Training

---
+++ myFirstProgram/myFirstProgram.cs
@@ -0,0 +1,31 @@
+﻿//First Program in Visual Studio
+
+using System;
+
+namespace myFirstProgram
+{
+    class myFirstProgram
+    {
+        static int factorial(int n)
+        {
+            if (n < 0)
+            {
+                return -1;
+            }
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+
+            return n * factorial(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Hello World!");
+            Console.WriteLine(factorial(x));
+
+        }
+    }
+}
